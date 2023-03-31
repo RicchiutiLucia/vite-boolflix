bfc270e2980b795e13113405873434e49ab99cc9
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-31 13:52:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import axios from 'axios';
+import { store } from "./store.js"
+export default{
+  components:{
+    AppHeader,
+    AppMain,
+
+
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+
+</script>
+
+
+<template>
+
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+ 
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use './styles/partials/variables.scss' as *;
+
+</style>
---
+++ src/components/AppFilm.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use '../styles/general.scss'
+</style>
